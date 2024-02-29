b874b6c409201aadbe3d25206ad469f4019aaff3
Benjamin <732426@pdsb.net>
2024-02-29 12:16:31 -0500

    WIP backup 1

--- App.js
+++ App.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import { View, Button, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
 
 // Screen components
 function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Button title="Go to Screen One" onPress={() => navigation.navigate('ScreenOne')} />
-      <Button title="Go to Screen Two" onPress={() => navigation.navigate('ScreenTwo')} />
+    <View style={styles.homeContainer}>
+      <Image style={styles.homeCrescendoImage} source={require('./assets/images/crescendo.png')} />
+      
+      <View style={styles.homeNavigationButtonContainer}>
+        <Pressable style={[styles.homeNavigationButton, {backgroundColor: '#c3423f'}]} onPress={() => navigation.navigate('standsScreen')}>
+          <Text style={styles.homeNavigationButtonText}>Stands</Text>
+        </Pressable>
+
+        <Pressable style={[styles.homeNavigationButton, {backgroundColor: '#5bc0eb'}]} onPress={() => navigation.navigate('pitsScreen')}>
+          <Text style={styles.homeNavigationButtonText}>Pits</Text>
+        </Pressable>
+
+        <Pressable style={[styles.homeNavigationButton, {backgroundColor: '#959595'}]}>
+          <Text style={styles.homeNavigationButtonText}>Reset</Text>
+        </Pressable>
+      </View>
+
+      <StatusBar barStyle='light-content' />
     </View>
   );
 }
 
-function ScreenOne({ navigation }) {
+function StandsScreen() {
   return (
-    <View style={styles.container}>
-      <Text>You are on the first screen.</Text>
+    <View style={styles.standsScoutingContainer}>
+      <Text>Hang yourself</Text>
     </View>
   );
 }
 
-function ScreenTwo({ navigation }) {
+function PitsScreen() {
   return (
-    <View style={styles.container}>
+    <View style={styles.standsScoutingContainer}>
       <Text>You are on the second screen.</Text>
     </View>
   );
@@ -36,18 +51,88 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home', headerShown: false }} />
-        <Stack.Screen name="ScreenOne" component={ScreenOne} options={{ title: 'Screen One' }} />
-        <Stack.Screen name="ScreenTwo" component={ScreenTwo} options={{ title: 'Screen Two' }} />
+        <Stack.Screen name="homeScreen" component={HomeScreen} options={{title: 'Home', headerShown: false}} />
+        <Stack.Screen name="standsScreen" component={StandsScreen} options={{
+          title: 'Stands',
+          headerRight: () => (
+            <Pressable style={styles.headerResetButton}>
+              <Text style={[styles.headerResetButtonText, {color: '#fff'}]}>Reset</Text>
+            </Pressable>
+          ),
+
+          headerStyle: {
+            backgroundColor: '#211a1e',
+            borderBottomColor: '#fff',
+            borderWidth: 1,
+          },
+
+          headerTintColor: '#fff',
+      }} />
+        <Stack.Screen name="pitsScreen" component={PitsScreen} options={{ title: 'Pits' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  homeContainer: {
     flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#191919',
+  },
+  
+  homeCrescendoImage: {
+    flex: 1,
+    marginTop: '10%',
+    width: '80%',
+    height: '20%',
+    objectFit: 'contain',
+  },
+  
+  homeNavigationButtonContainer: {
+    flex: 1,
+    width: '80%',
+    justifyContent: 'space-evenly',
+  },
+
+  homeNavigationButton: {
+    height: '18%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+  },
+
+  homeNavigationButtonText: {
+    color: '#fff',
+    fontSize: 28,
+    textShadowOffset: {
+      height: 2,
+    },
+    textShadowColor: 'rgba(0, 0, 0, 0.5)',
+    textShadowRadius: 5,
+  },
+
+  headerResetButton: {
+    marginRight: '8%',
+    backgroundColor: '#959595',
+    width: '30%',
+    height: '50%',
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 15,
   },
+  
+  standsScoutingContainer: {
+    flex: 1,
+    backgroundColor: '#211a1e',
+  },
+
+  headerResetButtonText: {
+    color: '#fff',
+    textShadowOffset: {
+      height: 2,
+    },
+    textShadowColor: 'rgba(0, 0, 0, 0.5)',
+    textShadowRadius: 5, 
+  }
 });
